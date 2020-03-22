dcf4d93a23bfde43077a3723d01b27c8c53ee734
chistmas <61202496+chistmas@users.noreply.github.com>
2020-03-22 22:55:33 +0300

    cmd reading

--- 1_lab_1.py
+++ 1_lab_1.py
@@ -1,13 +1,4 @@
 import math
-arr = []   
-n = int(input("Enter number of elements : "))  
-for i in range(0, n): 
-    ele = int(input()) 
-    arr.append(ele) # adding the element 
-    
-st = int(input("Enter the first element:"))
-fin = int(input("Enter the last element:"))
-
 
 #razbienie na summy podmassivov
 def devision(ar, leng):
@@ -25,12 +16,12 @@ def devision(ar, leng):
 #summa s 0-go elementa do poslednego zadannogo
 def big_summa(ar, fi, leng):
     spis = 0
-    i = math.floor(st/leng)
-    while i + leng <= fi and i % leng == 0:
-        spis += devision(ar, leng)[math.floor(i / leng)]
-        i += leng
-    for j in range(i, fi):
-        spis += ar[j]
+    j = math.floor(st/leng)
+    while j + leng <= fi and j % leng == 0:
+        spis += devision(ar, leng)[math.floor(j / leng)]
+        j += leng
+    for k in range(j, fi):
+        spis += ar[k]
     return spis
 
 #konechnaja funkcija 
@@ -43,6 +34,35 @@ def check(ar, st, fi, leng):
 def division(ar, st, fi):
     leng = math.ceil(len(ar)**0.5) 
     return check(ar, st, fi, leng)
+
+arr = []   
+z = int(input("Choose how to input data: \n1 -- from text.txt\n2 -- manually\n"))
+if z == 1:
+    f = open('text.txt', 'r+')
+    ff=f.read()
+    ff = ff.replace(",","")
+    ff = ff.replace("\n", " ")
+    ff = ff.replace("["," ")
+    ff = ff.replace("]", " ")
+    ff = ff.split()
+    st = int(ff[-2])
+    fin = int(ff[-1])
+    arra = ff[:-2]
+    arr = []
+    for i in range(len(arra)):
+        t = int(arra[i])
+        arr.append(t)
+    f.close()
+
+elif z == 2:
+    n = int(input("Enter number of elements : "))  
+    for i in range(0, n): 
+        ele = int(input()) 
+        arr.append(ele) 
+    st = int(input("Enter the first element:"))
+    fin = int(input("Enter the last element:"))
+else:
+    print("You've chosen something wrong -_-")
 
 print(division(arr, st, fin))
     
--- 1_lab_2.py
+++ 1_lab_2.py
@@ -1,21 +1,47 @@
 import random
 import string
-a = list(range(4, 7))
-b = list(range(6, 10))
-size = 200
-i = 0
-f= open("filee.txt","w+")
-alphabet = list(string.ascii_letters)
-text = ""
+import sys
 
-while i < size * 1000 * 1000:
-    stroka = random.choice(a)
-    for j in list(range(stroka)):
-        slovo = random.choice(b)
-        i += slovo
-        for k in list(range(slovo)):
-            text += random.choice(alphabet)
-        text += " "
-    text += "\n"
-f.write(text)
-f.close()
+if __name__ == "__main__":
+    d = int(input("1--Enter manually \n2--No\n"))
+    slova = []
+    bukvy = []
+    if d == 1:
+        a = input("How many words:\n")
+        b = input("How many letters:\n")
+        razmer = int(input("Size?:\n"))
+        a = (a.replace(",","")).split()
+        b = (b.replace(",","")).split()
+        for i in range(2):
+            t = int(a[i])
+            z = int(b[i])
+            slova.append(t)
+            bukvy.append(z)
+    elif d == 2:
+        for i in range(2):
+            slova.append(int(sys.argv[1 + i]))
+            bukvy.append(int(sys.argv[3+i]))
+            razmer = int(sys.argv[5])                
+    else:
+        print("Your choice was wrong!!!")
+    
+    i = 0
+    f= open("filee.txt","w+")
+    alphabet = list(string.ascii_letters)
+    text = ""
+
+    while i < razmer * 1000 * 1000:
+        stroka = random.choice(slova)
+        for j in list(range(stroka)):
+            slovo = random.choice(bukvy)
+            i += slovo + 1
+            for k in list(range(slovo)):
+                text += random.choice(alphabet)
+            text += " "
+        text += "\n"
+    f.write(text)
+    f.close()
+
+
+
+
