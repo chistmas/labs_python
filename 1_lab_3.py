0e0ace3566b3998eb4a830da48270bedbcbbb5d3
chistmas <61202496+chistmas@users.noreply.github.com>
2020-03-25 18:54:04 +0300

    length

--- 1_lab_3.py
+++ 1_lab_3.py
@@ -2,9 +2,9 @@ FileName = open("sort.txt", 'r+')
 data = FileName.readlines()
 for i in range(len(data)):
     wor = data[i].split()
-    wor.sort()
+    wor = sorted(wor, key = len)
     data[i] = " ".join(wor)
-data.sort()
+data = sorted(data, key = len)
 for i in range(len(data)):
     FileName.write(data[i])
     FileName.write("\n")
