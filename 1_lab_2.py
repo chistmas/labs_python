242bfbcce52d551d98052014d400aa78d3784c32
chistmas <61202496+chistmas@users.noreply.github.com>
2020-03-27 23:29:36 +0300

    new

--- 1_lab_2.py
+++ 1_lab_2.py
@@ -2,17 +2,18 @@ import random
 import string
 import sys
 
-def printProgressBar (iter, total, pref = '', suf = '', dec = 1,
-                      leng = 100, fill = '█', printEnd = "\r"):
-    
+
+def printProgressBar(iter, total, pref='', suf='', dec=1,
+                     leng=100, fill='█', printEnd="\r"):
     percent = ("{0:." + str(dec) + "f}").format(100 * (iter / float(total)))
     filledLength = int(leng * iter // total)
     bar = fill * filledLength + '-' * (leng - filledLength)
-    print('\r%s |%s| %s%% %s' % (pref, bar, percent, suf), end = printEnd)
+    print('\r%s |%s| %s%% %s' % (pref, bar, percent, suf), end=printEnd)
     # Print New Line on Complete
-    if iter == total: 
+    if iter == total:
         print()
-        
+
+
 if __name__ == "__main__":
     d = int(input("1--Enter manually \n2--No\n"))
     slova = []
@@ -21,8 +22,8 @@ if __name__ == "__main__":
         a = input("How many words:\n")
         b = input("How many letters:\n")
         razmer = int(input("Size?:\n"))
-        a = (a.replace(",","")).split()
-        b = (b.replace(",","")).split()
+        a = (a.replace(",", "")).split()
+        b = (b.replace(",", "")).split()
         for i in range(2):
             t = int(a[i])
             z = int(b[i])
@@ -31,32 +32,45 @@ if __name__ == "__main__":
     elif d == 2:
         for i in range(2):
             slova.append(int(sys.argv[1 + i]))
-            bukvy.append(int(sys.argv[3+i]))
-            razmer = int(sys.argv[5])                
+            bukvy.append(int(sys.argv[3 + i]))
+            razmer = int(sys.argv[5])
     else:
         print("Your choice was wrong!!!")
-    
+
     i = 0
-    f= open("filee.txt","w+")
+    f = open("filee.txt", "w+")
     alphabet = list(string.ascii_letters)
     text = ""
-    l = len(list(range(razmer*1000*1000)))
-    printProgressBar(0, l, pref = 'Progress:', suf = 'Complete',
-                             leng = 50)
-    while i < razmer * 1000 * 1000:
-        printProgressBar(i, l, pref = 'Progress:', suf = 'Complete',
-                         leng = 50)
+    l = len(list(range(razmer * 1000 * 1000)))
+    printProgressBar(0, l, pref='Progress:', suf='Complete',
+                     leng=50)
+    dlina = razmer * 1000000
+    while i < dlina:
+        printProgressBar(i, l, pref='Progress:', suf='Complete',
+                         leng=50)
         stroka = random.choice(slova)
         for j in list(range(stroka)):
             slovo = random.choice(bukvy)
-            i += slovo + 1
-            for k in list(range(slovo)):
-                text += random.choice(alphabet)
-            text += " "
+            i += slovo
+            if i + 3 <= dlina:
+                for k in list(range(slovo)):
+                    text += random.choice(alphabet)
+                text += " "
+                i += 1
+            else:
+                for k in bukvy:
+                    if k == dlina - i + 2:
+                        k = 1
+                if k == 1:
+                    slovo = k
+                    i += k
+                    for e in list(range(slovo)):
+                        text += random.choice(alphabet)
+                else:
+                    text += random.choice(alphabet)
+                    i += k
+
         text += "\n"
+        i += 2
     f.write(text)
     f.close()
-
-
-
-
